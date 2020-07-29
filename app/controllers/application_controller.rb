980c4643995e088d2041dad3d07f120522e3316e
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-07-29 17:10:22 +0900

    CSS、ビューの表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :view_parts, only: [:index, :show]
-
+  # before_action :move_to_index, except: [:index, :show, :edit]
 
   def view_parts
     @user = "ユーザー名 (サインイン・マイページ)"
     @title = "My Web list"
   end
+
+  # def move_to_index
+  #   unless user_signed_in?
+  #     redirect_to action: :index
+  #   end
+  # end
+
 end
