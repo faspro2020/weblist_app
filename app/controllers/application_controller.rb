885fcd295ae7389cf186a8965be5950ceb7c8370
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-07-29 16:39:48 +0900

    ビューの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :view_parts, only: [:index, :show]
+
+
+  def view_parts
+    @user = "ユーザー名 (サインイン・マイページ)"
+    @title = "My Web list"
+  end
 end
