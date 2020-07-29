1b15acb14db6e2cae8dacef6f5c292da1a5d30f2
faspro2020 <66356762+faspro2020@users.noreply.github.com>
2020-07-29 17:58:18 +0900

    新規登録　ログイン機能の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :view_parts, only: [:index, :show]
   # before_action :move_to_index, except: [:index, :show, :edit]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 
   def view_parts
     @user = "ユーザー名 (サインイン・マイページ)"
--- db/schema.rb
+++ db/schema.rb
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_07_29_070204) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
